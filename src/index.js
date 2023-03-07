ffc9115d32cd2f5ae9c452595977b91dbba7427d
Todd Brannon <toddqbrannon@gmail.com>
2023-03-07 14:26:59 -0600

    add prod link in Home.js

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import './index.css';
 import App from './App';
 import { WorkoutsContextProvider } from './context/WorkoutsContext';
 
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
